0d04f937383078a7299eef84d41d84b08af9c1b6
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-15 18:14:23 +0200

    feat: add CacheModule integration and Prisma module setup in content service

--- backend/content-service/app.module.ts
+++ backend/content-service/app.module.ts
@@ -1,10 +1,20 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-ioredis';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 300,
+    }),
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
