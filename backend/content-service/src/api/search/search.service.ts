8cdc179c046bca039ef721b0945997a74a43276f
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-15 21:46:22 +0200

    feat: implement SearchService to index articles in Elasticsearch

--- backend/content-service/src/api/search/search.service.ts
+++ backend/content-service/src/api/search/search.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { Article } from '@prisma/client';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  private readonly index = 'articles';
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  async indexArticle(article: Article) {
+    return this.elasticsearchService.index({
+      index: this.index,
+      id: article.id.toString(),
+      document: {
+        id: article.id,
+        title: article.title,
+        content: article.content,
+        slug: article.slug,
+        authorId: article.authorId,
+        createdAt: article.createdAt,
+      },
+    });
+  }
+}
