6bea1b165d2eb451000f44ea5468bdd20e2ea571
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-16 16:33:11 +0200

    feat(auth-service): implement sign-in functionality and restructure authentication logic

--- backend/auth-service/src/api/auth/auth.controller.ts
+++ backend/auth-service/src/api/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/request/sign-in.dto';
 import { SignUpDto } from './dto/request/sign-up.dto';
+import { SignInResponseDto } from './dto/response/sign-in-response.dto';
 import { UserResponseDto } from './dto/response/user-response.dto';
 
 @ApiTags('1. Authentication')
@@ -12,13 +22,24 @@ export class AuthController {
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({ summary: 'Register a new user' })
-  @ApiResponse({
-    status: 201,
-    description: 'User successfully created. Now you can login.',
-    type: UserResponseDto,
-  })
-  @ApiResponse({ status: 409, description: 'Email already exists' })
+  @ApiResponse({ status: 201, type: UserResponseDto })
   signUp(@Body() dto: SignUpDto): Promise<UserResponseDto> {
     return this.authService.signUp(dto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Login user' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Returns Access Token. Refresh Token is set in httpOnly cookie.',
+    type: SignInResponseDto,
+  })
+  async signIn(
+    @Body() dto: SignInDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<SignInResponseDto> {
+    return this.authService.signIn(dto, res);
   }
 }
