b76c7201e44ea1fb21a4540c26ff45d806636337
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-16 15:48:10 +0200

    feat(auth-service): require fullName in User model and implement user registration with password hashing

--- backend/auth-service/src/api/auth/auth.service.ts
+++ backend/auth-service/src/api/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { PrismaService } from 'prisma/prisma.service';
+import { SignUpDto } from './dto/request/sign-up.dto';
+import { UserResponseDto } from './dto/response/user-response.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async signUp(dto: SignUpDto): Promise<UserResponseDto> {
+    const password = await bcrypt.hash(dto.password, 10);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password,
+        fullName: dto.fullName,
+      },
+    });
+
+    return {
+      id: newUser.id,
+      email: newUser.email,
+      fullName: newUser.fullName,
+      role: newUser.role,
+      createdAt: newUser.createdAt,
+    };
+  }
+}
