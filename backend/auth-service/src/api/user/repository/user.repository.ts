249134f5db38671dba13daa2a3a54a24c8f0b657
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-16 19:42:58 +0200

    feat(auth-service): implement user profile retrieval and user repository

---
+++ backend/auth-service/src/api/user/repository/user.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async updateUser(
+    userId: number,
+    data: Prisma.UserUpdateInput,
+  ): Promise<User> {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data,
+    });
+  }
+
+  async deleteUser(userId: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: { id: userId },
+    });
+  }
+}
