a013cbea23f026734870400f2c2d91dd199bcded
kashubyak Mykola <kashubyak514@gmail.com>
2025-11-16 20:14:42 +0200

    feat(auth-service): add update user profile functionality and DTO

--- backend/auth-service/src/api/user/user.controller.ts
+++ backend/auth-service/src/api/user/user.controller.ts
@@ -1,13 +1,16 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
+  Patch,
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CurrentUserId } from 'src/common/decorators/current-user.decorator';
 import { AccessTokenGuard } from '../auth/guards/access-token.guard';
+import { UpdateUserDto } from './dto/request/update-user.dto';
 import { UserResponseDto } from './dto/response/user-response.dto';
 import { UserService } from './user.service';
 
@@ -23,5 +26,18 @@ export class UserController {
   @ApiResponse({ status: 200, type: UserResponseDto })
   getMe(@CurrentUserId() userId: number): Promise<UserResponseDto> {
     return this.userService.getMe(userId);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Patch('me')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Update current user profile' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, type: UserResponseDto })
+  updateMe(
+    @CurrentUserId() userId: number,
+    @Body() dto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.updateMe(userId, dto);
   }
 }
--- backend/auth-service/src/api/user/user.service.ts
+++ backend/auth-service/src/api/user/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import bcrypt from 'bcryptjs/umd/types';
+import { UpdateUserDto } from './dto/request/update-user.dto';
 import { UserResponseDto } from './dto/response/user-response.dto';
 import { UserRepository } from './repository/user.repository';
 
@@ -12,6 +15,31 @@ export class UserService {
       throw new NotFoundException('User not found');
     }
 
+    return {
+      id: user.id,
+      email: user.email,
+      fullName: user.fullName,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+
+  async updateMe(userId: number, dto: UpdateUserDto): Promise<UserResponseDto> {
+    const dataToUpdate: Prisma.UserUpdateInput = {};
+    if (dto.password)
+      dataToUpdate.password = await bcrypt.hash(dto.password, 10);
+    if (dto.fullName) dataToUpdate.fullName = dto.fullName;
+
+    const updatedUser = await this.userRepository.updateUser(
+      userId,
+      dataToUpdate,
+    );
+
+    return this.buildUserResponse(updatedUser);
+  }
+
+  private buildUserResponse(user: User): UserResponseDto {
     return {
       id: user.id,
       email: user.email,
